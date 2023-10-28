cb3801ce60c9473cc424aa04227662cbb4893264
Pedro Vinicius Coelho belem <pevine6508@gmail.com>
2023-10-28 17:23:19 -0300

    feat: add types to Model/

--- Model/User/type.go
+++ Model/User/type.go
@@ -1,1 +1,20 @@
 package User
+
+type (
+	SCard struct {
+		Bank   string `json:"bank"`
+		Secret int    `json:"secret"`
+	}
+
+	SWallet struct {
+		Money int     `json:"money"`
+		Cards []SCard `json:"cards"`
+	}
+
+	SUser struct {
+		Id     int     `json:"id"`
+		Age    int     `json:"age"`
+		Name   string  `json:"name"`
+		Wallet SWallet `json:"wallet"`
+	}
+)
--- Model/fakeDB.go
+++ Model/fakeDB.go
@@ -1,1 +1,24 @@
 package Model
+
+import "awesomeProject/Model/User"
+
+var FakeDB = []User.SUser{
+	{
+		Id:   1,
+		Age:  20,
+		Name: "Pedro",
+		Wallet: User.SWallet{
+			Money: 0,
+			Cards: []User.SCard{
+				{
+					Bank:   "Inter",
+					Secret: 123,
+				},
+				{
+					Bank:   "C6",
+					Secret: 321,
+				},
+			},
+		},
+	},
+}
